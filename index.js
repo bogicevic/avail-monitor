c88cf3d177a6a23c1dbcadf99fdc827877398e56
bogicevic <zbogicevic@outlook.com>
2018-08-01 01:52:45 +0200

    Added the request stream handler

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const url = require('url');
+const { StringDecoder } = require('string_decoder');
 
 const server = http.createServer((req, res) => {
     const parsedUrl = url.parse(req.url, true);
@@ -10,10 +11,21 @@ const server = http.createServer((req, res) => {
 
     const method = req.method.toLowerCase();
 
-    res.end('Hello World\n');
-    
-    console.log(`Request: ${method} ${trimmedPath} ${JSON.stringify(queryStringObject)}`);
-    console.log(`Headers: ${JSON.stringify(headers)}`);
+    // handle the request stream
+    const decoder = new StringDecoder('utf-8');
+    let buffer = '';
+    req.on('data', data => {
+        buffer += decoder.write(data);
+    });
+
+    req.on('end', () => {
+        buffer += decoder.end();
+        res.end('Hello World\n');
+
+        console.log(`Request: ${method} ${trimmedPath} ${JSON.stringify(queryStringObject)}`);
+        console.log(`Headers: ${JSON.stringify(headers)}`);
+        console.log(`Payload: ${buffer}`);
+    });    
 });
 
 server.listen(3000, () => {
