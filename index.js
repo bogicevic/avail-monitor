0f9e596cc5c1da9097c769869bf304167592f260
bogicevic <zbogicevic@outlook.com>
2018-08-01 01:16:59 +0200

    WIP: Building up the structure

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ const url = require('url');
 const server = http.createServer((req, res) => {
     const parsedUrl = url.parse(req.url, true);
     const path = parsedUrl.pathname;
-    let trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    const headers = req.headers;
+    const queryStringObject = parsedUrl.query;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    const method = req.method.toLowerCase();
 
     res.end('Hello World\n');
-
-    console.log(`Request received on path: ${trimmedPath}`)
+    
+    console.log(`Request: ${method} ${trimmedPath} ${JSON.stringify(queryStringObject)}`);
+    console.log(`Headers: ${JSON.stringify(headers)}`);
 });
 
 server.listen(3000, () => {
