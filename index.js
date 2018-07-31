06828df3a30fee1d45f0705aacccf3e8d275363d
bogicevic <zbogicevic@outlook.com>
2018-07-31 23:31:49 +0200

    Added basic http server

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+    let trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    res.end('Hello World\n');
+
+    console.log(`Request received on ${trimmedPath}`)
+});
+
+server.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
