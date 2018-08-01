780ce3c5f9dbb409a3c558a78c98eaecbf70d980
bogicevic <zbogicevic@outlook.com>
2018-08-02 00:36:19 +0200

    Added basic routing and route handling

--- index.js
+++ index.js
@@ -8,26 +8,63 @@ const server = http.createServer((req, res) => {
     const headers = req.headers;
     const queryStringObject = parsedUrl.query;
     const trimmedPath = path.replace(/^\/+|\/+$/g, '');
-
+    
     const method = req.method.toLowerCase();
-
+    
     // handle the request stream
     const decoder = new StringDecoder('utf-8');
     let buffer = '';
     req.on('data', data => {
         buffer += decoder.write(data);
     });
-
+    
     req.on('end', () => {
         buffer += decoder.end();
-        res.end('Hello World\n');
+        
+        console.log(`\nRequest rcv: ${method} ${trimmedPath} ${JSON.stringify(queryStringObject)}`);
+        console.log(`Headers rcv: ${JSON.stringify(headers)}`);
+        console.log(`Payload rcv: ${buffer}\n`);
 
-        console.log(`Request: ${method} ${trimmedPath} ${JSON.stringify(queryStringObject)}`);
-        console.log(`Headers: ${JSON.stringify(headers)}`);
-        console.log(`Payload: ${buffer}`);
-    });    
+        let chosenHandler = typeof(router[trimmedPath]) !== 'undefined' 
+            ? router[trimmedPath] 
+            : handlers.notFound;
+        
+        const data = {
+            'trimmedPath': trimmedPath,
+            'queryStringObject': queryStringObject,
+            'method': method,
+            'headers': headers,
+            'payload': buffer,
+        };
+
+        chosenHandler(data, (statusCode, payload) => {
+            statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+            payload = typeof(payload) == 'object' ? payload : {};
+            let payloadString = JSON.stringify(payload);
+
+            res.writeHead(statusCode);
+            res.end(payloadString);
+
+            console.log(`Response: ${statusCode} ${payloadString}`);
+        });
+        
+    });
 });
 
 server.listen(3000, () => {
     console.log('Server listening on port 3000');
 });
+
+const handlers = {};
+
+handlers.sample = (data, callback) => {
+    callback(406, {'name': 'sample handler'});
+};
+
+handlers.notFound = (data, callback) => {
+    callback(404);
+};
+
+const router = {
+    'sample': handlers.sample,
+};
